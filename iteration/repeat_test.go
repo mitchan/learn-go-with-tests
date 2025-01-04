45a1ed4b5773ee9bbaab11cf789c7e5124327055
Francesco Casetta <francesco.casetta@gmail.com>
2025-01-04 18:00:09 +0100

    iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,38 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	t.Run("repeats a word 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("repeats a word 10 times", func(t *testing.T) {
+		repeated := Repeat("a", 10)
+		expected := "aaaaaaaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	ris := Repeat("c", 3)
+	fmt.Println(ris)
+	// Output: ccc
+}
