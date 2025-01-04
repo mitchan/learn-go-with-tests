cf8998774d2acc560c27218c6189259bf0dd39e1
Francesco Casetta <francesco.casetta@gmail.com>
2025-01-04 10:26:21 +0100

    added more languages

--- 001-hello-world/hello_test.go
+++ 001-hello-world/hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Francesco")
+		got := Hello("Francesco", "")
 		want := "Hello, Francesco"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Italian", func(t *testing.T) {
+		got := Hello("Francesco", "Italian")
+		want := "Ciao, Francesco"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Francesco", "French")
+		want := "Bonjour, Francesco"
 		assertCorrectMessage(t, got, want)
 	})
 }
